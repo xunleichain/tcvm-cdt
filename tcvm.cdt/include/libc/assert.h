c242548ee015f839a9a0d4d8e8072931b9929505
Pebble-999 <55172758+Pebble-999@users.noreply.github.com>
2019-09-26 16:49:02 +0800

    Update assert.h: delete __FILE__ macro
    Inconsistent bytecodes compiled in different environments

--- tcvm.cdt/include/libc/assert.h
+++ tcvm.cdt/include/libc/assert.h
@@ -5,7 +5,7 @@
 #ifdef NDEBUG
 #define	assert(x) (void)0
 #else
-#define assert(x) ((void)((x) || (__assert_fail(#x, __FILE__, __LINE__, __func__),0)))
+#define assert(x) ((void)((x) || (__assert_fail(#x, "wasm", __LINE__, __func__),0)))
 #endif
 
 #if __STDC_VERSION__ >= 201112L && !defined(__cplusplus)
